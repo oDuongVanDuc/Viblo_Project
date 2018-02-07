fbe118131f7beb3240fa527dc5ffc3a1c125eea3
duong.van.duc <>
2018-02-07 17:03:30 +0700

    add viblo project firebase authentication

---
+++ Viblo_Social_Auth/app/src/main/java/com/framgia/viblo_social_auth/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.framgia.viblo_social_auth
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+
+class MainActivity : AppCompatActivity() {
+
+    //Firebase references
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mAuth: FirebaseAuth? = null
+    //UI elements
+    private var tvFirstName: TextView? = null
+    private var tvLastName: TextView? = null
+    private var tvEmail: TextView? = null
+    private var tvEmailVerifiied: TextView? = null
+    var mContext : Context? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initialise()
+    }
+
+    private fun initialise() {
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+        tvFirstName = findViewById<View>(R.id.tv_first_name) as TextView
+        tvLastName = findViewById<View>(R.id.tv_last_name) as TextView
+        tvEmail = findViewById<View>(R.id.tv_email) as TextView
+        tvEmailVerifiied = findViewById<View>(R.id.tv_email_verifiied) as TextView
+        mContext = this.baseContext
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val mUser = mAuth!!.currentUser
+        if(mUser != null){
+        val mUserReference = mDatabaseReference!!.child(mUser!!.uid)
+            tvEmail!!.text = mUser.email
+            tvEmailVerifiied!!.text = mUser.isEmailVerified.toString()
+            mUserReference.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    tvFirstName!!.text = snapshot.child("firstName").value as String
+                    tvLastName!!.text = snapshot.child("lastName").value as String
+                }
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
+        }
+        else
+         Toast.makeText(mContext, "Cant not conect to Firebase real database", Toast.LENGTH_LONG).show()
+
+
+    }
+    //mAuth.signOut()
+}
